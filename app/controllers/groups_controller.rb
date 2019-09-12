e68cdad2fdebc8e7e87495722cffbdaf9b045121
shiminatsu <shimichan0728@icloud.com>
2019-09-12 20:10:36 +0900

    create-contoroller&download

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,4 @@
-class CreateGroups < ActiveRecord::Migration[5.0]
-  def change
-    create_table :groups do |t|
-      t.string :name, null: false
-      t.index :name, unique: true
-      t.timestamps
-    end
+class GroupsController < ApplicationController
+  def index
   end
 end
